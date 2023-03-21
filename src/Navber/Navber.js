d9de7f624496a50ec658590a95509eed829f8530
rafin8990 <rafin8990@gmail.com>
2023-03-22 02:17:23 +0600

    category loaded and footer added

--- src/Navber/Navber.js
+++ src/Navber/Navber.js
@@ -7,21 +7,21 @@ import Navbar from 'react-bootstrap/Navbar';
 const Navber = () => {
     return (
         <div>
-            <Navbar collapseOnSelect expand="lg">
+            <Navbar bg='success' collapseOnSelect expand="lg">
                 <Container>
-                    <Navbar.Brand href="/home">
+                    <Navbar.Brand className='text-white' href="/home">
                         <img src="" alt="" />
                         Hero Hotel
                     </Navbar.Brand>
                     <Navbar.Toggle aria-controls="responsive-navbar-nav" />
                     <Navbar.Collapse id="responsive-navbar-nav">
                         <Nav className="me-auto">
-                            <Nav.Link href="#features">Home</Nav.Link>
-                            <Nav.Link href="#pricing">Booking</Nav.Link>
+                            <Nav.Link className='text-white' href="#features">Home</Nav.Link>
+                            <Nav.Link className='text-white' href="#pricing">Booking</Nav.Link>
                         </Nav>
                         <Nav>
-                            <Nav.Link href="#deets">Login</Nav.Link>
-                            <Nav.Link eventKey={2} href="#memes">
+                            <Nav.Link className='text-white' href="#deets">Login</Nav.Link>
+                            <Nav.Link className='text-white' eventKey={2} href="#memes">
                                Dark Mode
                             </Nav.Link>
                         </Nav>
--- src/Pages/HomePage/Categories/Categories.js
+++ src/Pages/HomePage/Categories/Categories.js
@@ -1,20 +1,46 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { Button } from 'react-bootstrap';
 import Card from 'react-bootstrap/Card';
 import Col from 'react-bootstrap/Col';
 import Row from 'react-bootstrap/Row';
 
 const Categories = () => {
+
+    const [categories, setCategories] = useState([])
+
+    useEffect(() => {
+        fetch('http://localhost:5000/categories')
+            .then(res => res.json())
+            .then(data => setCategories(data))
+    }, [])
     return (
         <div className='mt-5'>
-        <div>
-            <h3 className='text-center'>Categories</h3>
-            <h1 className='text-center'>Get <span className='font-weight-bold text-success'>20% discount</span> In first Order</h1>
-            <p className='text-center'>Show All Categories</p>
-        </div>
-        <div className='mt-5'>
+            <div>
+                <h3 className='text-center'>Categories</h3>
+                <h1 className='text-center'>Get <span className='font-weight-bold text-success'>20% discount</span> In first Order</h1>
+                <p className='text-center'>Show All Categories</p>
+            </div>
+            <div className='mt-5 container'>
+                <Row xs={1} md={2} lg={3} className="g-4">
+                    {
+                        categories.map((category, idx) => <Col key={idx}>
+                            <Card>
+                                <Card.Img variant="top" src={category.categoryImage} />
+                                <Card.Body>
+                                    <Card.Title>{category.name}</Card.Title>
+                                    <Card.Text>
+                                        This is a longer card with supporting text below as a natural
+                                        lead-in to additional content. This content is a little bit
+                                        longer.
+                                    </Card.Text>
+                                    <Button variant='outline-success' className='w-100'>View Category</Button>
+                                </Card.Body>
+                            </Card>
+                        </Col>)
+                    }
+                </Row>
+            </div>
 
-        </div>
-            
         </div>
     );
 };
